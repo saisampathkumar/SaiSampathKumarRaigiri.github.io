dd50f3a76b317f1b625d0c226c736419d2278561
saisampathkumar <srtwr@mail.umkc.edu>
2019-12-07 02:14:55 -0600

    changes

--- script/script.js
+++ script/script.js
@@ -10,3 +10,46 @@ function scrollFunction() {
     document.getElementById("logo").style.fontSize = "35px";
   }
 }
+
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function getProjectModule(){
+  var PDiv = document.getElementById("PROJECTS");
+  document.documentElement.scrollTop = PDiv.offsetTop - 80;
+}
+
+function getHomeModule(){
+  var HDiv = document.getElementById("HOME");
+  document.documentElement.scrollTop = HDiv.offsetTop - 80;
+}
+
+function getskillsModule(){
+  var SDiv = document.getElementById("SKILLS");
+  document.documentElement.scrollTop = SDiv.offsetTop - 80;
+}
+
+function getResumeModule(){
+  var RDiv = document.getElementById("RESUME");
+  document.documentElement.scrollTop = RDiv.offsetTop-80;
+}
+
+function getContactModule(){
+  var CDiv = document.getElementById("CONTACTS");
+  document.documentElement.scrollTop = CDiv.offsetTop - 80;
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
